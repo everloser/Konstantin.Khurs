5c81f64947ff3ede5b8f67179d35293092227230
Konstantin Khurs <everloser12@gmail.com>
2016-03-04 12:36:46 +0200

    homework14 added

---
+++ homework14/Ex1.java
@@ -0,0 +1,55 @@
+package homework14;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+/**
+ * This program demonstrates
+ * парсинг test.json файла, с помощью парсера библиотеки jackson.
+ * создание export.json файла.
+ * @version
+ * @author
+ */
+public class Ex1
+	{
+
+		public static void main(String[] args)
+		{
+			ObjectMapper mapper = new ObjectMapper();
+			Root user = new Root();
+	          try
+	        	{
+	        		user = (Root) mapper.readValue(new File ("homework14/test.json"),
+	                    Root.class);
+	            }
+	          catch (IOException ex)
+	        	  {System.out.println("Error = " + ex.getMessage());}
+			
+	          System.out.println(user.toString());
+	          
+	          try
+				{
+					mapper.writerWithDefaultPrettyPrinter().writeValue(new File("homework14/export.json"), user);
+					System.out.println("json created!");
+				}
+	          catch (JsonGenerationException e)
+				{
+					System.out.println("JsonGenerationException " + e.getMessage());
+				}
+	          catch (JsonMappingException e)
+				{
+					System.out.println("JsonMappingException " + e.getMessage());
+				}
+	          catch (IOException e)
+				{
+					System.out.println("IOException " + e.getMessage());
+				}
+	          
+			
+		}
+
+	}
---
+++ homework14/People.java
@@ -0,0 +1,59 @@
+package homework14;
+
+
+public class People
+	{
+		private long id;
+        private String name;
+        private String surname;
+        private long age;
+        private boolean isDegree;
+		public long getId()
+			{
+				return id;
+			}
+		public void setId(long id)
+			{
+				this.id = id;
+			}
+		public String getName()
+			{
+				return name;
+			}
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+		public String getSurname()
+			{
+				return surname;
+			}
+		public void setSurname(String surname)
+			{
+				this.surname = surname;
+			}
+		public long getAge()
+			{
+				return age;
+			}
+		public void setAge(long age)
+			{
+				this.age = age;
+			}
+		
+		
+		public boolean isisDegree()
+			{
+				return isDegree;
+			}
+		public void setDegree(boolean isDegree)
+			{
+				this.isDegree = isDegree;
+			}
+		@Override
+		public String toString()
+			{
+				return "People [id=" + id + ", name=" + name + ", surname=" + surname + ", age=" + age + ", isDegree="
+						+ isDegree + "]";
+			}
+	}

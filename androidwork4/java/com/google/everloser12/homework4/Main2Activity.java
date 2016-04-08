2928d4ba3953be045e4e75e2dfda2f07ebe3024a
Konstantin Khurs <everloser12@gmail.com>
2016-04-08 12:48:41 +0200

    androidwork6 added

--- androidwork4/java/com/google/everloser12/homework4/Main2Activity.java
+++ androidwork4/java/com/google/everloser12/homework4/Main2Activity.java
@@ -2,14 +2,19 @@ package com.google.everloser12.homework4;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.ClipDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     private static String KEY_S = "text";
+    ImageView imageView, imageView2;
+
+
     public static void show(Activity activity, String text)
     {
         Intent intent = new Intent(activity,Main2Activity.class);
@@ -26,6 +31,49 @@ public class Main2Activity extends AppCompatActivity {
         Intent intent = getIntent();
         String text = intent.getStringExtra(KEY_S);
         textView.setText(text);
+        imageView = (ImageView) findViewById(R.id.imageViewClip);
+        imageView2 = (ImageView) findViewById(R.id.imageViewAnimated);
+        final ClipDrawable clipDrawable = (ClipDrawable) imageView.getDrawable();
+        clipDrawable.setLevel(200);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i<=50; i++)
+                {
+                  final int k = i;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            clipDrawable.setLevel(k * 200);
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(40);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+        imageView.setOnClickListener(listener);
+        imageView2.setOnClickListener(listener);
+
 
     }
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (imageView.getVisibility() == imageView.GONE)
+            {
+                imageView.setVisibility(imageView.VISIBLE);
+                imageView2.setVisibility(imageView2.GONE);
+            }
+            else {
+                imageView.setVisibility(imageView.GONE);
+                imageView2.setVisibility(imageView2.VISIBLE);
+            }
+        }
+    };
 }
--- androidwork4/java/com/google/everloser12/homework4/SuperMainActivity.java
+++ androidwork4/java/com/google/everloser12/homework4/SuperMainActivity.java
@@ -5,15 +5,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SuperMainActivity extends AppCompatActivity {
 
     private Button butt1, butt2, butt3, butt4;
+    private ImageView icon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_super_main);
+        icon = (ImageView) findViewById(R.id.imageView);
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animRotateIn_icon = AnimationUtils.loadAnimation(SuperMainActivity.this,
+                        R.anim.first_anim);
+                icon.startAnimation(animRotateIn_icon);
+            }
+        });
+
         butt1 = (Button) findViewById(R.id.button);
         butt2 = (Button) findViewById(R.id.button2);
         butt3 = (Button) findViewById(R.id.button3);
@@ -54,6 +69,7 @@ public class SuperMainActivity extends AppCompatActivity {
                 }
             }
             startActivity(intent);
+            overridePendingTransition(R.anim.transition_first,R.anim.transition_second);
         }
     };
 
